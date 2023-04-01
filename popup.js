9a840e7f8a9cb364573ebc43e50d9ed6d1823a04
Bsodoge <kashhh4u@gmail.com>
2023-04-01 20:30:12 +0100

    Add ability to disable/enable

--- popup.js
+++ popup.js
@@ -1,13 +1,45 @@
 let buttonToggle = document.getElementById('toggle');
+let checkBox = document.getElementById('checkbox');
 let isToggled = false;
+let settings = {
+    isToggled : isToggled,
+    isIntermediate: checkBox.checked
+};
 buttonToggle.addEventListener('click', () => browser.tabs.query({active:true,currentWindow:true},toggleExtension));
-
-function toggleExtension(tabs){
-    if(isToggled){
+checkBox.addEventListener('click', () => {
+    setSettings();
+    browser.tabs.query({active:true,currentWindow:true},sendMessage);
+});
+const changeButtonText = () => {
+    if(!isToggled){
         buttonToggle.innerText = "Activate";
     } else{
         buttonToggle.innerText = "Deactivate";
     }
-    isToggled = !isToggled;
-    browser.tabs.sendMessage(tabs[0].id, isToggled);
 }
+const setSettings = () => {
+    settings = {
+        isToggled : isToggled,
+        isIntermediate: checkBox.checked
+    }
+    browser.storage.local.set(settings);
+}
+const onOpen = () => {
+    browser.storage.local.get(settings).then((storageSettings) => {
+        checkBox.checked = storageSettings.isIntermediate;
+        isToggled = storageSettings.isToggled;
+        changeButtonText();
+        settings = storageSettings;
+    });
+    browser.tabs.query({active:true,currentWindow:true},sendMessage)
+}
+const sendMessage = (tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, settings);
+}
+const toggleExtension =(tabs) => {
+    isToggled = !isToggled;
+    changeButtonText();
+    setSettings();
+    sendMessage(tabs);
+}
+onOpen();
